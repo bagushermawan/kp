4127e3b801a042a3e90cfd0e2159bce9bc58fa23
bagushermawan <bagusherma123@gmail.com>
2020-05-13 05:10:24 +0700

    done show product+paginate

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -11,7 +11,7 @@ class ShopController extends Controller
 {
     public function index()
     {
-        $produks = Product::all();
+        $produks = Product::paginate(8);
         // dd($produks);
         return view("layouts.frontend-master", ["produks" => $produks]);
 
